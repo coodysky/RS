4cf2cdcbacbf8877f2f5710bf155d1a8af8ca4bf
Coody <qingwen.liao@ezbuy.com>
2017-02-27 18:54:49 +0800

    MsDemo

---
+++ MsForCustomer/ExampleController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MsForCustomer
+{
+    public class ExampleController : ApiController
+    {
+        [Route("KKK/FindExample")]
+        [HttpGet]
+        public string FindExample()
+        {
+            return "Example";
+        }
+    }
+}
---
+++ MsForCustomer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicroService4Net;
+
+namespace MsForCustomer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var microService = new MicroService(port: 14001);
+            microService.Run(args);
+        }
+    }
+}
