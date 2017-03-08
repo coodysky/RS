3f3d6e9910f940d62d3b2d26abac1b1cef40210d
Coody <qingwen.liao@ezbuy.com>
2017-03-08 18:55:12 +0800

    获得表结构

--- CommonProjects/GenDbModel/Program.cs
+++ CommonProjects/GenDbModel/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dapper;
 
 namespace GenDbModel
 {
@@ -10,6 +13,21 @@ namespace GenDbModel
     {
         static void Main(string[] args)
         {
+            using (var conn = new SqlConnection(ConfigurationManager.AppSettings["ConnectionStringForSqlServer"]))
+            {
+                string sql = "EXEC SP_HELP 'Package'";
+                var s = conn.QueryMultiple(sql);
+                s.Read();//第一个结果忽略不要
+                List<ColumnsDef> columns = s.Read<ColumnsDef>().ToList();
+            }
         }
+    }
+
+    internal class ColumnsDef
+    {
+        public string Column_name { set; get; }
+        public string Type { set; get; }
+        public int Length { set; get; }
+        public string Nullable { set; get; }
     }
 }
