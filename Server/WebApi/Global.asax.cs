36d03e00db6c614638cc29c287fea0d3d29ce81b
Coody <qingwen.liao@ezbuy.com>
2017-05-29 15:10:29 +0800

    改成webapi

--- Server/WebApi/Global.asax.cs
+++ Server/WebApi/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using Ching.Intranet;
 
 namespace WebApi
 {
@@ -12,6 +13,7 @@ namespace WebApi
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
