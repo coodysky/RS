d213c3604d47cadd3b2171089886d2f1b72eb4cb
Coody <coodysky@aliyun.com>
2017-06-08 23:49:20 +0800

    需求打标签

--- Server/WebApi/Controllers/RequirementController.cs
+++ Server/WebApi/Controllers/RequirementController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
@@ -51,16 +52,46 @@ namespace WebApi.Controllers
                 modelRequirement.CreateDate = DateTime.Now;
                 modelRequirement.UpdateDate = DateTime.Now;
 
-                string sqlForRequirementInsert = DbModels.Requirement.GetSqlForInsert(modelRequirement);
+                string sqlForRequirementInsertAndQueryIdentity = DbModels.Requirement.GetSqlForInsertAndQueryIdentity(modelRequirement);
 
-                conn.Execute(sqlForRequirementInsert);
+                //添加事务
+                IDbTransaction tran = conn.BeginTransaction();
+
+                int requirementId = conn.QuerySingle<int>(sqlForRequirementInsertAndQueryIdentity);
+
+                if (requirementId > 0)
+                {
+                    #region 插入Tag
+
+                    foreach (var tag in requirement.RequirementTags)
+                    {
+                        if (string.IsNullOrEmpty(tag.Tag))
+                            continue;
+
+                        DbModels.RequirementTag modelTag = new DbModels.RequirementTag();
+                        modelTag.RequirementId = requirementId;
+                        modelTag.Tag = tag.Tag;
+                        modelTag.CreateBy = customer.NickName;
+                        modelTag.UpdateBy = customer.NickName;
+                        modelTag.CreateDate = DateTime.Now;
+                        modelTag.UpdateDate = DateTime.Now;
+
+                        string sqlForTagInsert = DbModels.RequirementTag.GetSqlForInsert(modelTag);
+
+                        conn.Execute(sqlForTagInsert);
+                    }
+
+                    #endregion
+                }
+
+                tran.Commit();
             }
 
             return new RespEntity() { Code = 1, Message = "" };
         }
         
         [HttpPost]
-        public RespEntity GetRequirement(int requirementId)
+        public RespEntity GetRequirement(int requirementId, bool isNeedTags = false)
         {
             if (requirementId <= 0)
             {
@@ -79,9 +110,27 @@ namespace WebApi.Controllers
                 {
                     requirements = new List<Requirement>();
 
-                    var requirement = buildFromModel(modelRequirement);
+                    var requirement = buildApiModelForRequirement(modelRequirement);
                     if (requirement != null)
                     {
+                        List<DbModels.RequirementTag> modelTags =
+                        conn.Query<DbModels.RequirementTag>(
+                            DbModels.RequirementTag.GetSqlForSelect(
+                                string.Format("RequirementId = {0}", requirementId), null, 0)).ToList();
+
+                        List<RequirementTag> tags = new List<RequirementTag>();
+                        foreach (var modelTag in modelTags)
+                        {
+                            RequirementTag tag = buildApiModelForRequirementTag(modelTag);
+                            if (tag != null)
+                            {
+                                tags.Add(tag);
+                            }
+                        }
+
+                        if (tags != null && tags.Count > 0)
+                            requirement.RequirementTags = tags;
+
                         requirements.Add(requirement);
                     }
                 }
@@ -178,7 +227,7 @@ namespace WebApi.Controllers
                     requirements = new List<Requirement>();
                     foreach (var modelRequirement in modelRequirements)
                     {
-                        var requirement = buildFromModel(modelRequirement);
+                        var requirement = buildApiModelForRequirement(modelRequirement);
 
                         if (requirement != null)
                         {
@@ -210,7 +259,7 @@ namespace WebApi.Controllers
                     requirements = new List<Requirement>();
                     foreach (var modelRequirement in modelRequirements)
                     {
-                        var requirement = buildFromModel(modelRequirement);
+                        var requirement = buildApiModelForRequirement(modelRequirement);
 
                         if (requirement != null)
                         {
@@ -230,7 +279,7 @@ namespace WebApi.Controllers
             return new RespEntity() {Code = 1, Message = title};
         }
 
-        private Requirement buildFromModel(DbModels.Requirement modelRequirement)
+        private Requirement buildApiModelForRequirement(DbModels.Requirement modelRequirement)
         {
             if (modelRequirement == null)
             {
@@ -256,6 +305,27 @@ namespace WebApi.Controllers
             };
 
             return requirement;
+        }
+
+        private RequirementTag buildApiModelForRequirementTag(DbModels.RequirementTag modelTag)
+        {
+            if (modelTag == null)
+            {
+                return null;
+            }
+
+            RequirementTag tag = new RequirementTag()
+            {
+                RequirementTagId = modelTag.RequirementTagId,
+                RequirementId = modelTag.RequirementId,
+                Tag = modelTag.Tag,
+                CreateBy = modelTag.CreateBy,
+                UpdateBy = modelTag.UpdateBy,
+                CreateDate = modelTag.CreateDate.ToStringDate(),
+                UpdateDate = modelTag.UpdateDate.ToStringDate()
+            };
+
+            return tag;
         }
     }
 }
